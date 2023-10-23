7575f5c5e0a17a747a5c825e5da9c2979465afb6
LecawYe <146970886+LecawYe@users.noreply.github.com>
2023-10-22 20:37:50 -0700

    l

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock-Paper-Scissors
+// An implementation of the game
+// Yihan Wang <ywang942@ucsc.edu>
+// Oct 22
 
-// Constants
+//Explain the rules
+//Accepts user input
+    //Prompt user for input
+        //print R, P, S (Rock, Paper, Scissors)
+//Validating user input
+    //ensure input is R, P, S
+//Generate computer move
+    //Randomly generate R, P, S
+//Compare moves and determine the winner
+    //Check if user wins
+//Display the result
+    //Print result
+//Provide replay option
+    //Prompt user for replay
+        //print Y, N (Yes, No)
+//End the game
 
-// Functions
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
