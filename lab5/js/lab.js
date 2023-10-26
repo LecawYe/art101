6c635add09de077505173f595437fc8233e89f5e
LecawYe <146970886+LecawYe@users.noreply.github.com>
2023-10-26 11:22:51 -0700

    l

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,26 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Author: Yihan Wang
+ * Date: 26 October 2023
+**/
 
-// Constants
+// Define Variables
+make = "Volkswagen";
+model = "Taos";
+color = "silver";
+year = 2022;
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// Calculate
+age = 2023 - year;
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//Output
+//Note, that is WRITENLN with an "L", not WRITELINE
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
 // let's get this party started
 main();
