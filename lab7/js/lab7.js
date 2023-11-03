5f92c158d42360d42d02194aebb288bdea96777a
LecawYe <146970886+LecawYe@users.noreply.github.com>
2023-11-02 22:11:03 -0700

    l

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -5,38 +5,27 @@
  * (c) Copyright by Yihan Wang.
  **/
 
-// Create the main title
-var title = document.createElement('h1');
-title.textContent = 'Enter Your Name';
-document.body.appendChild(title);
-
-// Create the submit button
-var submitButton = document.createElement('button');
-submitButton.textContent = 'Submit';
-document.body.appendChild(submitButton);
-
-// Function to shuffle the characters in a string
-function shuffle(string) {
-    var array = string.split('');
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array.join('');
+function sortUserName(userName) {
+  // Remove spaces and convert to lowercase for case-insensitive sorting
+  var userNameArray = userName.replace(/\s+/g, '').toLowerCase().split('');
+  userNameArray.sort();
+  return userNameArray.join('');
 }
 
-
-// Function to sort the letters of the user's name
 function sortUserName() {
-  // Prompt user for their name
-  var userName = window.prompt("Hi! Waht's your name? Please enter your name.");
-  if (person !=null) {
-    var sortedName = userName.split('').sort().join('');
-    console.log("Here's your sorted name: " + sortedName);
+ 
 
-    var anagram = shuffle(userName);
-    console.log("And oh hey, I fixed your name. Your new one is: " + anagram);
+  // Prompt the user for their name
+  var userName = window.prompt("Please enter your name:");
+  
+
+  // Split the string into an array, sort it, and then join it back into a string
+  var sortedName = userName.split('').sort().join('');
+
+  // Return the sorted string
+  return sortedName;
 }
-};
+
+// Call the function and use document.writeln to output the sorted name
+var userSortedName = sortUserName();
+document.writeln("<div class='sorted-name'>Your sorted name is: " + userSortedName + "</div>");
