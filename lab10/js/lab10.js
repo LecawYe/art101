9357b18b740b26eb5720c6b5602aada011dcde51
LecawYe <146970886+LecawYe@users.noreply.github.com>
2023-11-17 00:36:49 -0800

    Update lab10.js

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -8,14 +8,18 @@
 */
 
 function generateRandomText() {
-    const phrases = ["Hello there!", "How are you?", "Nice to meet you!", "What's up?", "Have a great day!"];
-    const randomIndex = Math.floor(Math.random() * phrases.length);
-    return phrases[randomIndex];
-}
-
-$(document).ready(function() {
-    $("#generate-button").click(function() {
-        const dialogue = generateRandomText();
-        $("#output").text(dialogue);
-    });
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+  }
+// click listener for button
+$("#make-convo").click(function(){
+    const newText = generateRandomText();
+    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+    
 });
