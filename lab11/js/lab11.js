d2e475bcd7ec417e41aaa6c674747519d8119a29
LecawYe <146970886+LecawYe@users.noreply.github.com>
2023-11-20 00:44:15 -0800

    1

--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -8,21 +8,50 @@
 */
 
 // Function to sort the characters of a string in alphabetical order
+// Sorts the characters of a string in alphabetical order
 function sortString(inputString) {
-    // Convert the string to an array, sort it, and then join back into a string
     return inputString.split('').sort().join('');
-  }
-  
-  // jQuery to handle the click event on the button with id 'submit'
-  $("#submit").click(function() {
-    // Get the value of the input field with id 'user-name'
-    const userName = $("#user-name").val();
-  
-    // Sort the userName using the sortString function
-    const userNameSorted = sortString(userName);
-  
-    // Select the output div with id 'output' and set its HTML content
-    // This appends a new div with the sorted user name inside a paragraph
-    $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
-  });
-  
+}
+
+// An anagram function (a simple version for demonstration)
+function anagram(inputString) {
+    if (inputString.length <= 2) {
+        return inputString.length === 2 ? [inputString, inputString[1] + inputString[0]] : [inputString];
+    }
+
+    return inputString.split('').reduce((acc, letter, i) =>
+        acc.concat(anagram(inputString.slice(0, i) + inputString.slice(i + 1)).map(val => letter + val)), []);
+}
+
+// jQuery to handle the click event on the button with id 'submit'
+$("#submit").click(function() {
+    const inputSelectors = ["#user-name", "#additional-input1", "#additional-input2"]; // Add more selectors as needed
+    $("#output").empty(); // Clearing the output before appending new content
+    inputSelectors.forEach(selector => {
+        const value = $(selector).val();
+        const sortedValue = sortString(value);
+        const anagrams = anagram(value).join(", ");
+        $("#output").append('<div class="text"><p>Sorted: ' + sortedValue + '</p><p>Anagrams: ' + anagrams + '</p></div>');
+    });
+});
+
+// Additional buttons for different actions
+$("#action1").click(function() {
+    // Code for the first action (e.g., change background color)
+    $("body").css("background-color", "lightblue");
+});
+
+$("#action2").click(function() {
+    // Code for the second action (e.g., change text color)
+    $("body").css("color", "green");
+});
+
+// Implementing a "glitch" effect
+function glitchEffect() {
+    // Simple glitch effect: randomly change background color
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    $("body").css("background-color", randomColor);
+}
+
+// Trigger the glitch effect every 5 seconds
+setInterval(glitchEffect, 5000);
