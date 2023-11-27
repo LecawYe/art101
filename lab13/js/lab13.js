bdb94cec6ab6946baa571b9cd6337c827483194e
LecawYe <146970886+LecawYe@users.noreply.github.com>
2023-11-27 00:00:41 -0800

    lab13

--- lab13/js/lab13.js
+++ lab13/js/lab13.js
@@ -5,20 +5,23 @@
 */
 // In lab.js - top of the file
 function fizzBuzzBoom() {
-    let maxNum = parseInt(document.getElementById("maxNumber").value);
-    let fizzFactor = parseInt(document.getElementById("factorFizz").value);
-    let buzzFactor = parseInt(document.getElementById("factorBuzz").value);
-    let boomFactor = parseInt(document.getElementById("factorBoom").value);
+    let maxNum = parseInt(document.getElementById("maxNumber").value, 10);
+    let fizzFactor = parseInt(document.getElementById("factorFizz").value, 10);
+    let buzzFactor = parseInt(document.getElementById("factorBuzz").value, 10);
+    let boomFactor = parseInt(document.getElementById("factorBoom").value, 10);
 
-    let output = "";
+    let output = document.getElementById("output");
+    output.innerHTML = ''; // Clear previous results
+
     for (let i = 1; i <= maxNum; i++) {
-        let result = "";
+        let result = '';
 
-        if (i % fizzFactor === 0) result += "Fizz!";
-        if (i % buzzFactor === 0) result += "Buzz!";
-        if (i % boomFactor === 0) result += "Boom!";
+        if (i % fizzFactor === 0) result += 'Fizz';
+        if (i % buzzFactor === 0) result += 'Buzz';
+        if (i % boomFactor === 0) result += 'Boom';
 
-        output += (result || i) + "<br>";
+        let textNode = document.createElement('div');
+        textNode.innerHTML = result || i;
+        output.appendChild(textNode);
     }
-    document.getElementById("output").innerHTML = output;
 }
