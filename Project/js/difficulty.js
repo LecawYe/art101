604574a05de5266adbe5843b31f6240293bd09cd
LecawYe <146970886+LecawYe@users.noreply.github.com>
2023-11-27 22:22:27 -0800

    123

--- Project/js/difficulty.js
+++ Project/js/difficulty.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-  function startGame(difficulty) {
+  const startGame = (difficulty) => {
     localStorage.setItem('difficulty', difficulty);
     window.location.href = 'game.html';
-  }
+  };
 
   document.getElementById('easy').addEventListener('click', () => startGame('easy'));
   document.getElementById('medium').addEventListener('click', () => startGame('medium'));
