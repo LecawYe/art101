d920571cb3306622d466c32cdb01958bed079c0e
LecawYe <146970886+LecawYe@users.noreply.github.com>
2023-11-27 22:03:26 -0800

    l

--- Project/js/game.js
+++ Project/js/game.js
@@ -14,40 +14,62 @@ document.addEventListener('DOMContentLoaded', () => {
   const matchPercentageDisplay = document.getElementById('matchPercentage');
   const difficulty = localStorage.getItem('difficulty') || 'easy';
   let randomColor = {};
+  let gameTimer;
 
   function generateRandomColor() {
-    randomColor.red = Math.floor(Math.random() * 256);
-    randomColor.green = Math.floor(Math.random() * 256);
-    randomColor.blue = Math.floor(Math.random() * 256);
-    randomColorBox.style.backgroundColor = `rgb(${randomColor.red}, ${randomColor.green}, ${randomColor.blue})`;
+    // ... existing code to generate a random color
   }
 
   function calculateMatchPercentage(userColor) {
-    const redDiff = Math.abs(randomColor.red - userColor.red);
-    const greenDiff = Math.abs(randomColor.green - userColor.green);
-    const blueDiff = Math.abs(randomColor.blue - userColor.blue);
-    const difference = redDiff + greenDiff + blueDiff;
-    const maxDifference = 255 * 3;
-    return 100 - Math.floor((difference / maxDifference) * 100);
+    // ... existing code to calculate the match percentage
   }
 
-  function setupGame() {
-    generateRandomColor();
-    // Add any additional setup based on difficulty if needed
-    // For example, set a timer for Medium and Hard modes
+  function endGame() {
+    // Disable sliders and show the final match percentage
+    document.getElementById('red').disabled = true;
+    document.getElementById('green').disabled = true;
+    document.getElementById('blue').disabled = true;
+    const userColor = getUserColor();
+    const matchPercentage = calculateMatchPercentage(userColor);
+    matchPercentageDisplay.textContent = `Time's up! Match: ${matchPercentage}%`;
   }
 
-  document.getElementById('check').addEventListener('click', () => {
-    const userColor = {
+  function getUserColor() {
+    return {
       red: parseInt(document.getElementById('red').value, 10),
       green: parseInt(document.getElementById('green').value, 10),
       blue: parseInt(document.getElementById('blue').value, 10)
     };
+  }
+
+  function startTimer(duration) {
+    gameTimer = setTimeout(endGame, duration);
+  }
+
+  function setupGame() {
+    generateRandomColor();
+    switch(difficulty) {
+      case 'medium':
+        startTimer(300000); // 5 minutes
+        break;
+      case 'hard':
+        startTimer(60000); // 1 minute
+        break;
+      // No timer for 'easy' difficulty
+    }
+  }
+
+  document.getElementById('check').addEventListener('click', () => {
+    clearTimeout(gameTimer); // Stop the timer when the user checks the color
+    const userColor = getUserColor();
     const matchPercentage = calculateMatchPercentage(userColor);
     matchPercentageDisplay.textContent = `Match: ${matchPercentage}%`;
   });
 
-  document.getElementById('reset').addEventListener('click', setupGame);
+  document.getElementById('reset').addEventListener('click', () => {
+    clearTimeout(gameTimer);
+    setupGame(); // Reset the game
+  });
 
   setupGame(); // Initialize the game
 });
