1cbd9916b65fbf4c4eb4ae10e13b7b9ad099ba5e
LecawYe <146970886+LecawYe@users.noreply.github.com>
2023-11-27 22:37:08 -0800

    l

--- Project/js/game.js
+++ Project/js/game.js
@@ -1,94 +1,44 @@
-// sardar
-// megan
-//dinal
-// yihan
-// November 2023
-
-
-
-
-// 1. Create a function that takes in a number and returns the number times two. Then run the function and print the result.
 document.addEventListener('DOMContentLoaded', () => {
-  const easyButton = document.getElementById('easy');
-  const mediumButton = document.getElementById('medium');
-  const hardButton = document.getElementById('hard');
+  const randomColorBox = document.getElementById('randomColor');
   const checkButton = document.getElementById('check');
   const resetButton = document.getElementById('reset');
-  const randomColorBox = document.getElementById('randomColor');
   const matchPercentageDisplay = document.getElementById('matchPercentage');
-
-  let randomColor = {};
-  let userColor = { red: 0, green: 0, blue: 0 };
-  let timeLimit;
-
+  
   function generateRandomColor() {
-    randomColor.red = Math.floor(Math.random() * 256);
-    randomColor.green = Math.floor(Math.random() * 256);
-    randomColor.blue = Math.floor(Math.random() * 256);
+    const randomColor = {
+      red: Math.floor(Math.random() * 256),
+      green: Math.floor(Math.random() * 256),
+      blue: Math.floor(Math.random() * 256)
+    };
     randomColorBox.style.backgroundColor = `rgb(${randomColor.red}, ${randomColor.green}, ${randomColor.blue})`;
+    return randomColor;
   }
-
-  function setColorSliders() {
-    document.getElementById('red').value = userColor.red;
-    document.getElementById('green').value = userColor.green;
-    document.getElementById('blue').value = userColor.blue;
-    updateUserColorDisplay();
+  
+  function getUserColor() {
+    return {
+      red: document.getElementById('red').value,
+      green: document.getElementById('green').value,
+      blue: document.getElementById('blue').value
+    };
   }
-
-  function updateUserColorDisplay() {
-    userColor.red = document.getElementById('red').value;
-    userColor.green = document.getElementById('green').value;
-    userColor.blue = document.getElementById('blue').value;
-    let userColorBox = document.getElementById('userColor');
-    if (!userColorBox) {
-      userColorBox = document.createElement('div');
-      userColorBox.id = 'userColor';
-      userColorBox.style.width = '100px';
-      userColorBox.style.height = '100px';
-      randomColorBox.insertAdjacentElement('afterend', userColorBox);
-    }
-    userColorBox.style.backgroundColor = `rgb(${userColor.red}, ${userColor.green}, ${userColor.blue})`;
+  
+  function compareColors(randomColor, userColor) {
+    return randomColor.red === userColor.red && randomColor.green === userColor.green && randomColor.blue === userColor.blue;
   }
-
-  function calculateMatchPercentage() {
-    const redDiff = Math.abs(randomColor.red - userColor.red);
-    const greenDiff = Math.abs(randomColor.green - userColor.green);
-    const blueDiff = Math.abs(randomColor.blue - userColor.blue);
-    const difference = redDiff + greenDiff + blueDiff;
-    const maxDifference = 255 * 3;
-    return 100 - Math.floor((difference / maxDifference) * 100);
-  }
-
-  easyButton.addEventListener('click', () => {
-    timeLimit = Infinity; // No time limit for easy
-    generateRandomColor();
-  });
-
-  mediumButton.addEventListener('click', () => {
-    timeLimit = 300000; // 5 minutes for medium
-    generateRandomColor();
-  });
-
-  hardButton.addEventListener('click', () => {
-    timeLimit = 60000; // 1 minute for hard
-    generateRandomColor();
-  });
-
+  
+  let randomColor = generateRandomColor();
+  
   checkButton.addEventListener('click', () => {
-    const matchPercentage = calculateMatchPercentage();
-    matchPercentageDisplay.textContent = `Match: ${matchPercentage}%`;
+    const userColor = getUserColor();
+    if (compareColors(randomColor, userColor)) {
+      matchPercentageDisplay.textContent = 'Correct! 🎉';
+    } else {
+      matchPercentageDisplay.textContent = 'Incorrect. Try again!';
+    }
   });
-
+  
   resetButton.addEventListener('click', () => {
-    userColor = { red: 0, green: 0, blue: 0 };
-    setColorSliders();
+    randomColor = generateRandomColor();
     matchPercentageDisplay.textContent = '';
-  });
-
-  // Set up the initial state
-  generateRandomColor();
-  setColorSliders();
-  ['red', 'green', 'blue'].forEach(color => {
-    document.getElementById(color).addEventListener('input', updateUserColorDisplay);
   });
 });
